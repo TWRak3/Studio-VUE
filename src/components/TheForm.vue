6f52c8c6fc1586f8a74537674ee69b167a53ea59
a.nidelea <nideleaantonio9@gmail.com>
2025-01-21 16:28:41 +0100

    terzo push

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,11 +1,86 @@
 <template>
-  <div class="container">
-    test
-  </div>
+  <form @submit.prevent="submitForm">
+    <div class="container">
+      <h1>Form function</h1>
+      <div class="">
+        <label for="user-name">Your Name</label>
+        <input id="user-name" name="user-name" type="text" v-model="userName" />
+      </div>
+      <div class="">
+        <label for="user-age">Your Age (Years)</label>
+        <input id="user-age" name="user-age" type="number" v-model="userAge" />
+      </div>
+      <div class="">
+        <label for="information">How did you hear about us?</label>
+        <select id="information" name="information" v-model="information">
+          <option value="google">Google</option>
+          <option value="social-media">Social media</option>
+          <option value="sponsor">Sponsor</option>
+        </select>
+      </div>
+      <div class="">
+        <h2>What are you interested in?</h2>
+        <div>
+          <input id="what" name="what" type="checkbox" value="news" v-model="what" />
+          <label for="what">News</label>
+        </div>
+        <div>
+          <input id="what" name="what" type="checkbox" value="tutorials" v-model="what" />
+          <label for="what">Tutorials</label>
+        </div>
+        <div>
+          <input id="what" name="what" type="checkbox" value="gaming" v-model="what" />
+          <label for="what">Gaming</label>
+        </div>
+      </div>
+      <div class="">
+        <h2>How do you learn?</h2>
+        <div>
+          <input id="learn" name="learn" type="radio" value="video" v-model="learn" />
+          <label for="learn">Video Courses</label>
+        </div>
+        <div>
+          <input id="learn" name="learn" type="radio" value="blogs" v-model="learn" />
+          <label for="learn">Blogs</label>
+        </div>
+        <div>
+          <input id="learn" name="learn" type="radio" value="other" v-model="learn" />
+          <label for="learn">Other</label>
+        </div>
+      </div>
+      <div>
+        <button>Save Data</button>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      userName: '',
+      userAge: '',
+      information: '',
+      what: [],
+      learn: [],
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log('Username: ' + this.userName);
+      this.userName = '';
+      console.log('User age: ' + this.userAge);
+      this.userAge = '';
+      console.log('Information: ' + this.information);
+      this.information = '';
+      console.log('What are you interested in: ' + this.what);
+      this.what = [];
+      console.log('How do you learn: ' + this.learn);
+      this.learn = [];
+    }
+  }
+}
 </script>
 
 <style scoped>
